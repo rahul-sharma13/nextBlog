6239043426314a8dbd0efae86ac4037e4532c31b
rahul-sharma13 <rahulharit13@gmail.com>
2024-11-21 20:05:11 +0530

    initial project setup

---
+++ api/index.js
@@ -0,0 +1,55 @@
+import dotenv from "dotenv";
+import connectDb from "./src/db/index.js";
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config({
+  path: "./env",
+});
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("server is up index!");
+});
+
+// middleware setup
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "50mb" })); // limit of acceptance of json data
+app.use(
+  express.urlencoded({ extended: true, limit: "50mb", parameterLimit: 50000 })
+);
+app.use(express.static("public"));
+app.use(cookieParser());
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal server error";
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
+// connecting to the database
+connectDb()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log("Error: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Port is running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB CONNECTION FAILED!! : ", err);
+  });
